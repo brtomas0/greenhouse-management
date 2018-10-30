76d88c262202a678304bd1e9a71d5b12d0136b4d
brtomas0 <brtomas0@gmail.com>
2018-10-29 21:50:48 -0500

    export files added, started with importing files

--- gui_test.py
+++ gui_test.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+from tkinter import font
+from tkinter import ttk
 import os, time
 
 todays_date = time.strftime("%x")
@@ -15,7 +17,7 @@ info_list =	 (	"Plant Id",
 
 action_colors = {	"Pot Info" :		("#000", "#222", "#fff"),
 					"Into Soil" : 		("#050", "#272", "#fff"),
-					"Into Greenhouse" : ("#070", "#292", "#000"),
+					"Into Greenhouse" : ("#070", "#292", "#fff"),
 					"Into Pots" : 		("#0a0", "#2c2", "#000"),
 					"Begin Flowering" : ("#0d0", "#2f2", "#000"),
 					"End Flowering" : 	("#8d0", "#af2", "#000"),
@@ -23,8 +25,35 @@ action_colors = {	"Pot Info" :		("#000", "#222", "#fff"),
 					"Harvested" : 		("#dd0", "#ff2", "#000")}
 
 actions_in_order = ["Pot Info", "Into Soil", "Into Greenhouse", "Into Pots", "Begin Flowering", "End Flowering", "End Maturation", "Harvested"]
+export_filename = "Greenhouse_Summary.csv"
+import_filename = "New_Greenhouse_Data.csv"
 data_folder = "Tub_data"
 
+class ContinueQuestion():
+	def __init__(self, master):
+		self.master = master
+		self.frame = tk.Frame(master)
+		self.frame.pack()
+		self.continue_answer = None
+		self.button_font = font.Font(size = 48)
+
+		self.continue_button = tk.Button(self.frame, text = "Continue?", command = self.answerTrue, font = self.button_font, width = 24, height = 6)
+		self.continue_button.pack(side = "left")
+		self.quit_button = tk.Button(self.frame, text = "Quit", fg = "red", command = self.answerFalse, font = self.button_font, width = 24, height = 6)
+		self.quit_button.pack(side = "right")
+
+		self.master.wait_window()
+
+	def answerTrue(self):
+		self.continue_answer = True
+		self.master.destroy()
+
+	def answerFalse(self):
+		self.continue_answer = False
+		self.master.destroy()
+
+	def getAnswer(self):
+		return self.continue_answer
 
 class Program():
 
@@ -54,21 +83,23 @@ class Program():
 		self.tub_columns = 10
 		self.app_rows = 8
 		self.app_columns = 12
-		self.button_width = 15
-		self.button_height = 3
+		self.button_width = 18
+		self.button_height = 4
+		self.pot_font = font.Font(size = 12)
+		self.label_font = font.Font(size = 12)
 
 		self.buttonInitilize()
-		self.resetTub()
+		self.importTub()
 
 	def buttonInitilize(self):
-		self.current_state_label = tk.Label(self.frame, textvariable = self.action_state)
+		self.current_state_label = tk.Label(self.frame, textvariable = self.action_state, font = self.label_font)
 		self.current_state_label.grid(row = 0, column = 3)
-		self.current_date_label = tk.Label(self.frame, text = todays_date)
+		self.current_date_label = tk.Label(self.frame, text = todays_date, font = self.label_font)
 		self.current_date_label.grid(row = 0, column = 4)
-		self.update_button = tk.Button(self.frame, text = "Update", width = self.button_width, height = self.button_height, command = self.importFile)
-		self.update_button.grid(row = 0, column = 0)
-		self.reset_button = tk.Button(self.frame, text = "Reset", width = self.button_width, height = self.button_height, command = self.resetTub)
-		self.reset_button.grid(row = 1, column = 0)
+		self.export_button = tk.Button(self.frame, text = "Export", width = self.button_width, height = self.button_height, command = self.exportMaster, font = self.label_font)
+		self.export_button.grid(row = 0, column = 0)
+		self.import_button = tk.Button(self.frame, text = "Import", width = self.button_width, height = self.button_height, command = self.importMaster, font = self.label_font)
+		self.import_button.grid(row = 1, column = 0)
 
 		start_row = 1
 		for i in range(self.number_of_greenhouses):
@@ -78,7 +109,8 @@ class Program():
 												height = self.button_height,
 												variable = self.greenhouse_variable,
 												value = i,
-												command = self.updateGreenhouse)
+												command = self.updateGreenhouse,
+												font = self.pot_font)
 			self.greenhouse_list[i].grid(		row = (int(i / 10) + start_row),
 												column = (i % 10 + 3))
 
@@ -91,7 +123,8 @@ class Program():
 										variable = self.tub_variable,
 										value = i,
 										background = "#aaa",
-										command = self.updateTub)
+										command = self.updateTub,
+										font = self.pot_font)
 			self.tub_list[i].grid(		row = (int(i / 10) + start_row),
 										column = (i % 10 + 3))
 
@@ -101,7 +134,8 @@ class Program():
 			self.pot_list[i].configure(	textvariable = self.pot_text[i],
 										command = lambda a=i: self.updatePot(a),
 										width = self.button_width, 
-										height = self.button_height)
+										height = self.button_height,
+										font = self.pot_font)
 			self.pot_list[i].grid(		row = (i % self.tub_rows + start_row),
 										column = (int( i / self.tub_rows ) + 3))
 
@@ -115,18 +149,19 @@ class Program():
 											foreground 			= action_colors[action][2], 
 											activeforeground 	= action_colors[action][2], 
 											width 				= self.button_width*2, 
-											height 				= self.button_height,
+											height 				= int(self.button_height),
 											command 			= lambda a=action: self.updateAction(a),
 											variable 			= self.action_state,
-											value				= action)
+											value				= action,
+											font 				= self.label_font)
 			self.action_button[i].grid(		row = (i+2),
 											column = 0)
 
 	def updateGreenhouse(self):
-		self.importFile()
+		self.importTub()
 
 	def updateTub(self):
-		self.importFile()
+		self.importTub()
 
 	def updatePot(self, i):
 		#print("Pot Number is: %s"%(i+1))
@@ -197,17 +232,17 @@ class Program():
 		self.action_state.set(action)
 		print(self.action_state.get())
 
-	def importFile(self):
+	def importTub(self):
 		gh = self.greenhouse_variable.get()
 		tub = self.tub_variable.get()
 		#"%s\nPot %02d\n%s"%(self.plant_names[i].get(), i + 1, self.plant_dates[i].get())
 		self.resetTub()
 		filename =  "GH%01d%s-%s.csv"%(int(gh / 2) + 1, "E" * (gh % 2) + "W" * ((gh +1) % 2), tub + 1)
 		file_path = data_folder + "\\" + filename
-		print("importing " + filename)
+		#print("importing " + filename)
 		
 		if filename not in os.listdir(data_folder):
-			print("Not Found")
+			print("File Does not exist in %s"%data_folder)
 			return
 
 		with open(file_path) as ifile:
@@ -225,10 +260,53 @@ class Program():
 		self.action_state.set(info_list[0])
 		return
 
+	def exportMaster(self):
+		master_file = open(export_filename, "w")
+		titles = [info_list[0]] + ["Greenhouse","Tub Number"] + [i for i in info_list[1:]]
+		master_file.write(",".join(titles) + "\n")
+		for file in os.listdir(data_folder):
+			gh = self.greenhouse_variable.get()
+			gh_text = "GH%01d%s"%(int(gh / 2) + 1, "E" * (gh % 2) + "W" * ((gh +1) % 2))
+			tub_text = "%s"%(self.tub_variable.get() + 1)
+
+			with open(data_folder + "\\" + file) as tub_file:
+				tub_file.readline()
+				for line in tub_file:
+					split_line = line.split(",")
+					new_line = [split_line[0]] + [gh_text, tub_text] + [i for i in split_line[1:]]
+					#print(new_line)
+					master_file.write(",".join(new_line))
+		master_file.close()
+
+	def importMaster(self):
+		if import_filename not in os.listdir():
+			print("File does not exist\n")
+			return
+
+		ifile = open(import_filename, "r")
+
+		first_line = ifile.readline().strip().split(",")
+		titles = [first_line[0]] + first_line[3:]
+		if tuple(titles) != info_list:
+			print("Titles are not correct, should be:\n\n%s\n"%",".join(info_list))
+			return
+
+		if ContinueQuestion(tk.Tk()).getAnswer():
+			print("File import commencing from %s"%import_filename)
+			for line in ifile:
+				split_line = line.strip().split(",")
+				filename = "-".join(split_line[1:3])
+				print(filename)
+		# Need to merge the files, overwriting old data with new data
+
+		ifile.close()
+
+
 def main():
 	root = tk.Tk()
-	Program(root)
+	greenhouse_gui = Program(root)
 	root.mainloop()
+	greenhouse_gui.exportMaster()
 
 
 if __name__ == '__main__':
